4dfb962adf4700f20f19b1e833ca5f455c1a506c
Daniel Leuck <dan@ikayzo.com>
2023-02-23 17:50:34 +0900

    Mode Comparable extension public

--- Sources/KiCore/Comparable+.swift
+++ Sources/KiCore/Comparable+.swift
@@ -5,7 +5,7 @@
 //  Created by Daniel Leuck on 2023/02/23.
 //
 
-extension Comparable {
+public extension Comparable {
     func clamp(_ limits: ClosedRange<Self>) -> Self {
         return min(max(self, limits.lowerBound), limits.upperBound)
     }
