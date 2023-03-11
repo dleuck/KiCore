232ad4fcabcd701713777905fc95ebc3f88af7d9
dleuck <dan@ikayzo.com>
2023-03-11 19:02:08 +0900

    fixed Double.rounded()

--- Sources/KiCore/Double+.swift
+++ Sources/KiCore/Double+.swift
@@ -15,7 +15,7 @@ extension Double: SoftNullable {
     }
             
     /// Rounds the double to decimal places value
-    func round(places:Int) -> Double {
+    func rounded(places:Int) -> Double {
         let divisor = pow(10.0, Double(places))
         return (self * divisor).rounded() / divisor
     }
