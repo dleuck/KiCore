4bcb0e92af640f5664c84442e2ef0e6c3b99921b
dleuck <dan@ikayzo.com>
2023-03-11 18:43:05 +0900

    Added round with places param

--- Sources/KiCore/Double+.swift
+++ Sources/KiCore/Double+.swift
@@ -8,7 +8,16 @@
 import Foundation
 
 extension Double: SoftNullable {
+    
+    /// Conforms to SoftNullable - we use isNaN as the equivalent of a soft null
     public var isNull: Bool {
         return self.isNaN
     }
+            
+    /// Rounds the double to decimal places value
+    func round(_ places:Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
 }
+ 
